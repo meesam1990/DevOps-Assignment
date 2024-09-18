c8ddab7b18c73474e383f8622a7d561c89defbc0
meesam1990 <38735315+meesam1990@users.noreply.github.com>
2024-09-18 20:24:17 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-# Use s3 bucket to store terraform state files
-terraform {
-  backend "s3" {
-    bucket = "meesam-backend-bucket"  
-    key    = "terraform/state"
-    region = "ap-south-1"
-  }
-}
